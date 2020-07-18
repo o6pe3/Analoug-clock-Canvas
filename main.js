d9087e59b094cbd9a4a9695e1427ab65784167a8
o6pe3 <gkshot@gmail.com>
2020-07-18 21:06:47 +0400

    bug fix

--- main.js
+++ main.js
@@ -10,16 +10,17 @@ const xCenterClock = canvas.width / 2
 const yCenterClock = canvas.height / 2
 const radiusClock = xCenterClock - 10
 
-// numbers radius & hands length
+// numbers radius & hands length & tickSize
 const radiusNum = radiusClock - 10
 const lengthSeconds = radiusNum - 10
 const lengthMinutes = radiusNum - 20
 const lengthHour = lengthMinutes / 1.5
+const tickSize = 5
 
 // util functions
 const radOverDeg = Math.PI / 180
 const halfPI = Math.PI / 2
-const secToRad = (sec) => -6 * sec * radOverDeg
+const secToRad = (sec) => 6 * sec * radOverDeg
 const getKx = (sec) => Math.cos(secToRad(sec) + halfPI)
 const getKy = (sec) => -Math.sin(secToRad(sec) + halfPI)
 
@@ -37,54 +38,46 @@ function displayCanvas() {
   ctx.closePath()
 
   // clock ticks
-  let radiusPoint
   ctx.beginPath()
   ctx.lineWidth = 3
-  for (let tm = 0; tm < 60; tm++) {
-    if (tm % 5 == 0) {
-      radiusPoint = 10
-    } else {
-      radiusPoint = 5
-    }
-    const xPointM = xCenterClock + radiusNum * getKx(tm)
-    const yPointM = yCenterClock - radiusNum * getKy(tm)
+  for (let m = 0; m < 60; m++) {
+    let tickLength = m % 5 == 0 ? 2 * tickSize : tickSize
+    const xPointM = xCenterClock + radiusNum * getKx(m)
+    const yPointM = yCenterClock + radiusNum * getKy(m)
 
     ctx.moveTo(xPointM, yPointM)
-    ctx.lineTo(
-      xPointM + radiusPoint * getKx(tm),
-      yPointM - radiusPoint * getKy(tm)
-    )
+    ctx.lineTo(xPointM + tickLength * getKx(m), yPointM + tickLength * getKy(m))
     ctx.stroke()
   }
   ctx.closePath()
 
   // put numbers
   ctx.beginPath()
-  for (let th = 1; th <= 12; th++) {
+  for (let h = 1; h <= 12; h++) {
     ctx.font = 'bold 2rem sans-serif'
-    let xText = xCenterClock + (radiusNum - 30) * getKx(5 * th)
-    let yText = yCenterClock + (radiusNum - 30) * getKy(5 * th)
-    if (th <= 9) {
-      ctx.fillText(th, xText - 5, yText + 10)
+    let xText = xCenterClock - (radiusNum - 30) * getKx(5 * h)
+    let yText = yCenterClock + (radiusNum - 30) * getKy(5 * h)
+    if (h <= 9) {
+      ctx.fillText(h, xText - 5, yText + 10)
     } else {
-      ctx.fillText(th, xText - 15, yText + 10)
+      ctx.fillText(h, xText - 15, yText + 10)
     }
     ctx.fill()
   }
   ctx.closePath()
 
-  // draw jands
+  // draw hands
   const d = new Date()
-  const sec = d.getSeconds() // sec angle
+  const sec = d.getSeconds()
   const minute = 6 * d.getMinutes() + sec / 10 // min angle
-  const hour = 30 * d.getHours() + minute / 12 // hour angle
+  const hour = 30 * d.getHours() + minute / 10 // hour angle
 
   // draw seconds hand
   ctx.beginPath()
   ctx.strokeStyle = secondsHandColor
   ctx.moveTo(xCenterClock, yCenterClock)
   ctx.lineTo(
-    xCenterClock + lengthSeconds * getKx(sec),
+    xCenterClock - lengthSeconds * getKx(sec),
     yCenterClock + lengthSeconds * getKy(sec)
   )
   ctx.stroke()
@@ -96,7 +89,7 @@ function displayCanvas() {
   ctx.lineWidth = 3
   ctx.moveTo(xCenterClock, yCenterClock)
   ctx.lineTo(
-    xCenterClock + lengthMinutes * getKx(minute / 6),
+    xCenterClock - lengthMinutes * getKx(minute / 6),
     yCenterClock + lengthMinutes * getKy(minute / 6)
   )
   ctx.stroke()
@@ -107,22 +100,21 @@ function displayCanvas() {
   ctx.lineWidth = 5
   ctx.moveTo(xCenterClock, yCenterClock)
   ctx.lineTo(
-    xCenterClock + lengthHour * getKx(hour / 30), // Math.cos(halfPI - hour * radOverDeg),
-    yCenterClock + lengthHour * getKy(hour / 30) // Math.sin(halfPI - hour * radOverDeg)
+    xCenterClock - lengthHour * getKx(hour / 6),
+    yCenterClock + lengthHour * getKy(hour / 6)
   )
   ctx.stroke()
   ctx.closePath()
 
   // draw center
   ctx.beginPath()
-  ctx.lineWidth = 3
   ctx.arc(xCenterClock, yCenterClock, 5, 0, 2 * Math.PI, true)
   ctx.fill()
   ctx.closePath()
 }
 
 window.setInterval(function () {
-  let d = new Date()
-  document.getElementById('clock').innerHTML = d.toLocaleTimeString()
+  let date = new Date()
+  document.getElementById('clock').innerHTML = date.toLocaleTimeString()
   displayCanvas()
 }, 1000)
